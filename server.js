3980280246421dd82ba5b9051316233c70d16b68
Mathieu Gosbee <mail@matbee.com>
2012-01-31 01:14:59 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
-/**
- * Showing with the Express framwork http://expressjs.com/
- * Express must be installed for this sample to work
- */
-
 var tropoapi = require('tropo-webapi');
 var express = require('express');
 var app = express.createServer(); 
 
-/**
- * Required to process the HTTP body
- * req.body has the Object while req.rawBody has the JSON string
- */
+
 app.configure(function(){
 	app.use(express.bodyParser());
 });
@@ -20,20 +12,13 @@ app.get('/', function(req, res) {
 });
 
 app.post('/', function(req, res){
-	// Create a new instance of the TropoWebAPI object.
 	var tropo = new tropoapi.TropoWebAPI();
-	// Use the say method https://www.tropo.com/docs/webapi/say.htm
 	tropo.say("Hello Mariam!.");
 
-	// Demonstrates how to use the base Tropo action classes.
 	var say = {"value":"Do you love Mathieu Gosbee? Say yes or no!"};
-	// var choices = new tropo.Choices("[5 DIGITS]");
 	var choices = {"value":"yes(1, yes), no(2, no)"};
 
-	// Action classes can be passes as parameters to TropoWebAPI class methods.
-	// use the ask method https://www.tropo.com/docs/webapi/ask.htm
 	tropo.ask(choices, 3, false, null, "foo", null, true, say, 5, null);
-	// use the on method https://www.tropo.com/docs/webapi/on.htm
 	tropo.on("continue", null, "/answer", true);
 
     res.send(tropoapi.TropoJSON(tropo));
